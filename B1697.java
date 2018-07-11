bc6affa9018d2efa3c4c5d147ca1ad97c44b3075
SteadyHagi <thkang0107@gmail.com>
2018-07-12 01:31:50 +0900

    날짜 및 주석

--- B1697.java
+++ B1697.java
@@ -9,6 +9,15 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class B1697 {
+	/**
+	 * 숨바꼭질
+	 * bfs
+	 * 20180710
+	 * bfs의 특성을 잘 알면 쉽게 풀 수 있는 문제. 
+	 * bfs를 좀 더 능숙하게 쓸 수 있도록 노력해야겠다. 
+	 *  + 여러 조건들을 잘 세워야 하고, Point를 통해 bfs의 레벨을 숫자와 매칭하는 것이 진정한 포인트! 
+	 */
+	
 	static Queue<Point> q = new LinkedList<Point>();
 	static int start; 
 	static int goal;
